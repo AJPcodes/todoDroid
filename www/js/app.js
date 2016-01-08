f1d4bb9940e115db03839cf01beb43f2832456ba
AJP <allenatmc@gmail.com>
2016-01-07 22:43:57 -0600

    shake to shuffle working

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('todo', ['ionic'])
+angular.module('todo', ['ionic', 'ngCordova'])
 /**
  * The Projects factory handles saving and loading projects
  * from local storage, and also lets us save and load the
@@ -50,7 +50,7 @@ angular.module('todo', ['ionic'])
   }
 })
 
-.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
+.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate, $ionicPlatform, $cordovaDeviceMotion) {
 
   // A utility function for creating a new project
   // with the given projectTitle
@@ -119,6 +119,22 @@ angular.module('todo', ['ionic'])
     $scope.taskModal.show();
   };
 
+  $scope.randomizeTasks = function() {
+    var newList = [];
+
+   for (i = 0; i < $scope.activeProject.tasks.length; i++){
+    if (Math.random() * 10 > 5) {
+      newList.push($scope.activeProject.tasks[i]);
+    } else {
+      newList.unshift($scope.activeProject.tasks[i]);
+    }
+
+   }
+
+   $scope.activeProject.tasks = newList;
+
+  };
+
   $scope.deleteTask = function(taskIndex) {
     $scope.activeProject.tasks.splice(taskIndex, 1);
 
@@ -149,6 +165,131 @@ angular.module('todo', ['ionic'])
       }
     }
   });
+
+
+  /*SHAKE DETECTION CODE GOES HERE!*/
+  document.addEventListener("deviceready", function () {
+
+   // watch Acceleration options
+    $scope.options = {
+        frequency: 1001, // Measure every 100ms
+        deviation : 25  // We'll use deviation to determine the shake event, best values in the range between 25 and 30
+    };
+
+    // Current measurements
+    $scope.measurements = {
+        x : null,
+        y : null,
+        z : null,
+        timestamp : null
+    }
+
+    // Previous measurements
+    $scope.previousMeasurements = {
+        x : null,
+        y : null,
+        z : null,
+        timestamp : null
+    }
+
+    // Watcher object
+    $scope.watch = null;
+
+    // Start measurements when Cordova device is ready
+
+        //Start Watching method
+        $scope.startWatching = function() {
+
+ // Clean previous measurements
+            $scope.previousMeasurements = {
+                    x: null,
+                    y: null,
+                    z: null
+            }
+            // Device motion configuration
+            $scope.watch = $cordovaDeviceMotion.watchAcceleration($scope.options);
+
+            // Device motion initilaization
+            $scope.watch.then(null, function(error) {
+                console.log('Error');
+            },function(result) {
+                // Set current data
+                $scope.measurements.x = result.x;
+                $scope.measurements.y = result.y;
+                $scope.measurements.z = result.z;
+                $scope.measurements.timestamp = result.timestamp;
+
+                // Detecta shake
+                console.log('trying to detect shake');
+                $scope.detectShake(result);
+
+            });
+
+        };
+
+
+
+        // Stop watching method
+        $scope.stopWatching = function() {
+            $scope.watch.clearWatch();
+        };
+
+        $scope.processShake = function(){
+          console.log('Shake detected'); // shake detected
+          $scope.randomizeTasks();
+        };
+
+        // Detect shake method
+        $scope.detectShake = function(result) {
+
+
+            //Object to hold measurement difference between current and old data
+            var measurementsChange = {};
+
+            // Calculate measurement change only if we have two sets of data, current and old
+            if ($scope.previousMeasurements.x !== null) {
+                measurementsChange.x = Math.abs($scope.previousMeasurements.x, result.x);
+                measurementsChange.y = Math.abs($scope.previousMeasurements.y, result.y);
+                measurementsChange.z = Math.abs($scope.previousMeasurements.z, result.z);
+            }
+
+            // If measurement change is bigger then predefined deviation
+            if (measurementsChange.x + measurementsChange.y + measurementsChange.z >= $scope.options.deviation) {
+
+                $scope.stopWatching();
+                $cordovaDeviceMotion.clearWatch($scope.watch) // Stop watching because it will start triggering like hell
+                $scope.processShake();
+
+                setTimeout(function(){
+                  $scope.startWatching();
+                }, 5000);
+                // Clean previous measurements after succesfull shake detection, so we can do it next time
+
+
+            } else {
+                // On first measurements set it as the previous one
+                $scope.previousMeasurements = {
+                    x: result.x,
+                    y: result.y,
+                    z: result.z
+                }
+            }
+
+        }
+
+      $scope.startWatching();
+
+    }); //end ready device
+
+    $scope.$on('$ionicView.beforeLeave', function(){
+        $scope.watch.clearWatch(); // Turn off motion detection watcher
+    });
+
+
+
+  /*SHAKE DETECTION CODE GOES HERE!*/
+
+
 
 })
 
